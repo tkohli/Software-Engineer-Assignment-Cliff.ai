004829fc78e3fbfeb535eac8087c57ba0ec38c14
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-23 16:28:56 +0530

    Create products spiders.py

---
+++ SEAssignment/SEAssignment/spiders/products spiders.py
@@ -0,0 +1,39 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "products"
+
+    def start_requests(self):
+        urls = [
+            'https://www.net-a-porter.com/en-in/shop/clothing/tops?pageNumber=1'
+        ]
+        for i in range(2, 27):
+            urls.append(
+                "https://www.net-a-porter.com/en-in/shop/clothing/tops?pageNumber="+str(i))
+        for i in range(1, 27):
+            urls.append(
+                "https://www.net-a-porter.com/en-in/shop/shoes?pageNumber="+str(i))
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # title = response.css('title::text').get()
+        # yield {'title': title}
+        # for quote in range(50):
+        # items['Titles'] = title
+        for quote in response.css('div.ProductItem24__p'):
+            category = response.css(".FilterTags52__tag::text").get()
+            if category == "Tops":
+                category = 'Topwear'
+            else:
+                category = 'Footwear'
+            yield {
+                'name': quote.css('.ProductItem24__designer::text').get(),
+                'brand': quote.css('.ProductItem24__name::text').get(),
+                'price': quote.css('span::text')[2].get(),
+                'sale_price': quote.css('span::text')[2].get(),
+                'image_url': quote.css('img').xpath('@src').get(),
+                'product_page_url': response.css('.ProductGrid52 a::attr(href)').get(),
+                'product_category': category
+            }
